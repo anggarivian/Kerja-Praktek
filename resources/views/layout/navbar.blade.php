71a867531a8c6d82771c8b829faa8428280baff9
anggarivian.11 <anggarivian.11@gmail.com>
2023-05-26 13:43:32 +0700

    Add Template Dashboard

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -7,18 +7,15 @@
         </div>
         <div>
           <a class="navbar-brand brand-logo" href="{{asset('template/index.html')}}">
-            <img src="{{asset('template/images/logo.svg')}}" alt="logo" />
-          </a>
-          <a class="navbar-brand brand-logo-mini" href="{{asset('template/index.html')}}">
-            <img src="{{asset('template/images/logo-mini.svg')}}" alt="logo" />
+            <img src="{{asset('template/images/SITASU.png')}}" alt="logo" />
           </a>
         </div>
       </div>
-      <div class="navbar-menu-wrapper d-flex align-items-top"> 
+      <div class="navbar-menu-wrapper d-flex align-items-top">
         <ul class="navbar-nav">
           <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-            <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">John Doe</span></h1>
-            <h3 class="welcome-sub-text">Your performance summary this week </h3>
+            <h1 class="welcome-text">Selamat Datang, <span class="text-black fw-bold">Tresna Komala</span></h1>
+            <h3 class="welcome-sub-text">Sistem Informasi Tabungan SD Negeri Sukarame </h3>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
@@ -34,13 +31,9 @@
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
               <div class="dropdown-header text-center">
                 <img class="img-md rounded-circle" src="{{asset('template/images/faces/face8.jpg')}}" alt="Profile image">
-                <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
-                <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                <p class="mb-1 mt-3 font-weight-semibold">Tresna Komala</p>
+                <p class="fw-light text-muted mb-0">ktreskomala@gmail.com</p>
               </div>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i> My Profile <span class="badge badge-pill badge-danger">1</span></a>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i> Messages</a>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i> Activity</a>
-              <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i> FAQ</a>
               <a class="dropdown-item"><i class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign Out</a>
             </div>
           </li>
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,25 +1,65 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="{{asset('template/index.html')}}">
-              <i class="mdi mdi-grid-large menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
-            </a>
-          </li>
-          <li class="nav-item nav-category">UI Elements</li>
-          <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="menu-icon mdi mdi-floor-plan"></i>
-              <span class="menu-title">UI Elements</span>
-              <i class="menu-arrow"></i> 
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/buttons.html')}}">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/typography.html')}}">Typography</a></li>
-              </ul>
-            </div>
-          </li>
+  <ul class="nav">
+    <li class="nav-item">
+          <a class="nav-link" href="#"">
+        <i class="mdi mdi-grid-large menu-icon"></i>
+        <span class="menu-title">Beranda</span>
+      </a>
+    </li>
+    <li class="nav-item nav-category">Pengelolaan Tabungan</li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#transaksi" aria-expanded="false" aria-controls="transaksi">
+        <i class="menu-icon mdi mdi-cash-register"></i>
+        <span class="menu-title">Transaksi</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="transaksi">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item "> <a class="nav-link" href="{{asset('template/pages/ui-features/buttons.html')}}">Stor Tabungan</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Tarik Tabungan</a></li>
         </ul>
-      </nav>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#pengajuan" aria-expanded="false" aria-controls="pengajuan">
+        <i class="menu-icon mdi mdi-cash-refund"></i>
+        <span class="menu-title">Pengajuan  </span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="pengajuan">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item "> <a class="nav-link" href="{{asset('template/pages/ui-features/buttons.html')}}">Tertunda</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Riwayat</a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#laporan" aria-expanded="false" aria-controls="laporan">
+        <i class="menu-icon mdi mdi-file-document"></i>
+        <span class="menu-title">Laporan</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="laporan">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item "> <a class="nav-link" href="{{asset('template/pages/ui-features/buttons.html')}}">Transaksi</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Pengajuan</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Siswa</a></li>
+          <li class="nav-item"> <a class="nav-link" href="{{asset('template/pages/ui-features/dropdowns.html')}}">Petugas</a></li>
+        </ul>`
+      </div>
+    </li>
+    <li class="nav-item nav-category">Pengelolaan Pengguna</li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="menu-icon mdi mdi-account-group"></i>
+        <span class="menu-title">Data Siswa</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">
+        <i class="menu-icon mdi mdi-account-tie"></i>
+        <span class="menu-title">Data Petugas</span>
+      </a>
+    </li>
+  </ul>
+</nav>
